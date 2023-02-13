cf4b9f3a59c6f03747bd76fe116c8eda0e9874df
peskyboyz <adam.peskett@yahoo.com>
2023-02-12 19:27:03 -0500

    Fixed button to CustomizeTableState Moved the buttons on MenuState

--- source/CustomizeTableState.hx
+++ source/CustomizeTableState.hx
@@ -4,31 +4,29 @@ import flixel.FlxState;
 import flixel.ui.FlxButton;
 import flixel.FlxG;
 
-class CustomizeTableStae extends FlxState
+class CustomizeTableState extends FlxState
 {
     //initializing the variables
     var _btnPlay:FlxButton;
 
     override public function create():Void
-        {
-            
-            //adding the button for the user to click to start the game
-            _btnPlay = new FlxButton(50,50, "Play",clickPlay);
-            _btnPlay.screenCenter();
-            add(_btnPlay);
-            super.create();
-        }
+    {  
+        //adding the button for the user to click to start the game
+        _btnPlay = new FlxButton(50,50, "Play",clickPlay);
+        _btnPlay.screenCenter();
+        add(_btnPlay);
+        super.create();
+    }
 
         //play button is clicked
-        function clickPlay():Void
-            {
-                // switched state from current to PlayState
-                FlxG.switchState(new PlayState());
-            }
+    function clickPlay():Void
+    {
+        // switched state from current to PlayState
+        FlxG.switchState(new PlayState());
+    }
 
-            override public function update(elapsed:Float):Void
-            
-                {
-                    super.update(elapsed);
-            }
+    override public function update(elapsed:Float):Void
+    {
+            super.update(elapsed);
+    }
 }
--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.util.FlxColor;
 import flixel.FlxSprite;
 import flixel.input.FlxAccelerometer;
 import flixel.FlxState;
@@ -13,36 +14,35 @@ class MenuState extends FlxState
     var _btnCustomize:FlxButton;
 
     override public function create():Void
-        {
-            
-            //adding the button for the user to click to start the game
-            _btnPlay = new FlxButton(50,50, "Play",clickPlay);
-            _btnPlay.screenCenter();
-            add(_btnPlay);
+    {
+        camera.bgColor = FlxColor.WHITE;
+        //adding the button for the user to click to start the game
+        _btnPlay = new FlxButton(50,50, "Play",clickPlay);
+        _btnPlay.screenCenter(X);
+        add(_btnPlay);
 
-            //adding the button for the user to click to set up the row and column for the graph
-            _btnCustomize = new FlxButton(50,70, "Customize The Graph",customizeTable);
-            _btnCustomize.screenCenter();
-            add(_btnCustomize);
-            super.create();
-        }
+        //adding the button for the user to click to set up the row and column for the graph
+        _btnCustomize = new FlxButton(50,110, "Customize The Graph",customizeTable);
+        _btnCustomize.screenCenter(X);
+        add(_btnCustomize);
+        super.create();
+    }
 
-        //play button is clicked
-        function clickPlay():Void
-            {
-                // switched state from current to PlayState
-                FlxG.switchState(new PlayState());
-            }
-        
-        function customizeTable():Void 
-            {
-                // switched state from current to CustomizeTableState  //ERRORS
-                FlxG.switchState(new CustomizeTableState());
-            }
+    //play button is clicked
+    function clickPlay():Void
+    {
+        // switched state from current to PlayState
+        FlxG.switchState(new PlayState());
+    }
+    
+    function customizeTable():Void 
+    {
+        // switched state from current to CustomizeTableState  //ERRORS
+        FlxG.switchState(new CustomizeTableState());
+    }
 
-            override public function update(elapsed:Float):Void
-            
-                {
-                    super.update(elapsed);
-            }
+    override public function update(elapsed:Float):Void
+    {
+            super.update(elapsed);
+    }
 }
